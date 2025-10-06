dd4f34a304f4b64e87b58165b41f0f2ba92b7224
Luiz <luizhabluetzel@hotmail.ch>
2025-10-06 17:30:38 +0200

    parent class connectable  implemented

--- HS25_SoCo-group_069_a1/smart_house.py
+++ HS25_SoCo-group_069_a1/smart_house.py
@@ -1,1 +1,26 @@
-print("test")
+#methods "Connectable"
+def connect(thing, ip):
+    thing["ip"]  = ip
+    thing["connected"] = True
+
+def disconnect(thing):
+    thing["connected"] = False
+
+def is_connected(thing):
+    return thing["connected"]
+
+def new_connectable(connected: bool, ip: str):
+    return {
+        connected: connected,
+        ip: ip
+    }
+    
+#parent class "Connectable"
+Connectable = {
+    "_classname": "Connectable",
+    "_parent": None,
+    "_new": new_connectable,
+    "connect": connect,
+    "disconnect": disconnect,
+    "is_connected": is_connected,
+}
